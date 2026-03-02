8a15b9f955f8c235c849057ae51d7ae010397efa
ahmadkhan562 <m.ishtiaqahmad86@gmail.com>
2026-03-02 13:03:49 +0500

    added comments and proper indentation

---
+++ task1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>  // For input/output
+using namespace std;
+
+int main() {
+    // This program prints student details
+
+    /* 
+       Multi-line comment:
+       Printing Name, Roll No, Course, and Semester
+       using multiple cout statements
+    */
+    
+    cout << "Name: Ishtiaq Ahmad" << endl;   // Print name
+    cout << "Roll No: 24263" << endl;        // Print roll number
+    cout << "Course: Software Construction" << endl;  // Print course
+    cout << "Semester: 5th" << endl;         // Print semester
+
+    return 0;  // End of program
+}
