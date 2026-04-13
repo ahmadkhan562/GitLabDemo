6786cc623d794f8924cdde4275e33c27c70f0a0c
ahmadkhan562 <m.ishtiaqahmad86@gmail.com>
2026-04-13 12:12:50 +0500

    Add files via upload

---
+++ lab assignment.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[10];
+
+    cout << "Enter 10 elements:\n";
+    for (int i = 0; i < 10; i++) {
+        cin >> arr[i];
+    }
+
+    // Bubble Sort
+    for (int i = 0; i < 9; i++) {
+        for (int j = 0; j < 9 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+
+    cout << "Sorted array (Ascending Order):\n";
+    for (int i = 0; i < 10; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
