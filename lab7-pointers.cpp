c16d8344fb30848b39be2f536362d744c15bd923
ahmadkhan562 <m.ishtiaqahmad86@gmail.com>
2026-04-13 11:48:33 +0500

    Lab 7 Pointers

---
+++ lab7-pointers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[5] = {10, 20, 30, 40, 50};
+    int *ptr = arr;   // Pointer points to first element
+
+    cout << "Array elements using pointer arithmetic:\n";
+
+    for (int i = 0; i < 5; i++) {
+        cout << *(ptr + i) << " ";
+    }
+
+    return 0;
+}
